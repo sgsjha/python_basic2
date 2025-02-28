41b76dcd5c1995f6a7f90afef8c9bf01b345eaeb
sgsjha <sarthakjhaa11@gmail.com>
2025-02-28 12:07:19 +0000

    ffirst commit

---
+++ main.py
@@ -0,0 +1,70 @@
+# Pytho Basics
+
+# Print function
+print("Hello World!")
+
+# Variables
+
+
+# String
+name = "John"
+print(name)
+
+
+
+# Float
+height = 1.75
+print(height)   
+
+# Boolean
+employed = True
+print(employed)
+
+#Lists
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+
+#Dictionary
+person1 = {"name": "John", "age": 25, "employed": True}
+print(person1)
+
+#key value pairs
+print(person1["name"])
+
+
+num1 = "3"
+num2 = "2"
+
+print("werseesr = ", num1 + num2)
+
+#User Input
+
+#number1 = input("Enter 1st number: ")
+#print(number1)
+
+
+#Age checking
+
+# input funciton is expecting an int
+#age = int(input("Enter your age : "))
+# if age is less then 13 then print you are a kid
+# if age is 13 to 17, then print you are a teenager
+# if age is 18 to 60, then you are an adult
+# if anything else (more than 60, you are a senior citizen)
+
+# For Loop
+for i in range(0, 9):
+    print(i)
+
+
+# While Loop
+boolean = input("Enter T/F: ")
+while boolean == "T":
+    print("You entered True")
+    boolean = input("Enter T/F: ")
+else:
+    print("You entered False")
+
+
+
